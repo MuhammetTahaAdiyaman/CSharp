8ebef52b7b44d573fa0fd0fd2b93e8e91b71abc8
Muhammet Taha Adıyaman <68828015+MuhammetTahaAdiyaman@users.noreply.github.com>
2024-05-02 20:09:26 +0300

    Add files via upload

---
+++ Odev3/Odev3/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Odev3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            maskedTextBox2 = new MaskedTextBox();
+            comboBox1 = new ComboBox();
+            label5 = new Label();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            label6 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(38, 58);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 25);
+            label1.TabIndex = 0;
+            label1.Text = "isim:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(38, 95);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(147, 32);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(38, 189);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(147, 32);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(38, 152);
+            label2.Margin = new Padding(5, 0, 5, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 25);
+            label2.TabIndex = 2;
+            label2.Text = "soyisim:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(38, 256);
+            label3.Margin = new Padding(5, 0, 5, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 25);
+            label3.TabIndex = 4;
+            label3.Text = "TC:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(38, 352);
+            label4.Margin = new Padding(5, 0, 5, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 25);
+            label4.TabIndex = 5;
+            label4.Text = "Telefon:";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(38, 295);
+            maskedTextBox1.Mask = "00000000000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(147, 32);
+            maskedTextBox1.TabIndex = 6;
+            maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(38, 390);
+            maskedTextBox2.Mask = "(999) 000-0000";
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(147, 32);
+            maskedTextBox2.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Konya", "Ankara", "İstanbul", "İzmir" });
+            comboBox1.Location = new Point(38, 488);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(147, 33);
+            comboBox1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(38, 460);
+            label5.Margin = new Padding(5, 0, 5, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(59, 25);
+            label5.TabIndex = 9;
+            label5.Text = "Şehir:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(38, 577);
+            button1.Name = "button1";
+            button1.Size = new Size(147, 37);
+            button1.TabIndex = 10;
+            button1.Text = "Kaydet";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(615, 95);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(559, 379);
+            listBox1.TabIndex = 11;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(282, 58);
+            label6.Margin = new Padding(5, 0, 5, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(129, 25);
+            label6.TabIndex = 12;
+            label6.Text = "Doğum Tarihi:";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(282, 95);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(264, 32);
+            dateTimePicker1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1257, 750);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label6);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(comboBox1);
+            Controls.Add(maskedTextBox2);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 14F);
+            Margin = new Padding(5);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private MaskedTextBox maskedTextBox1;
+        private MaskedTextBox maskedTextBox2;
+        private ComboBox comboBox1;
+        private Label label5;
+        private Button button1;
+        private ListBox listBox1;
+        private Label label6;
+        private DateTimePicker dateTimePicker1;
+    }
+}
