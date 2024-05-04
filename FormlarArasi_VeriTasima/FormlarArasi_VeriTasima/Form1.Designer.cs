0aac858725817b029ee0aa96a113ec550fc1859e
Muhammet Taha Adıyaman <68828015+MuhammetTahaAdiyaman@users.noreply.github.com>
2024-05-04 20:31:36 +0300

    Add files via upload

---
+++ FormlarArasi_VeriTasima/FormlarArasi_VeriTasima/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace FormlarArasi_VeriTasima
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(225, 42);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(192, 32);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(259, 183);
+            button1.Name = "button1";
+            button1.Size = new Size(115, 38);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(225, 91);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(192, 32);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(225, 145);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(192, 32);
+            textBox3.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(686, 416);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Font = new Font("Segoe UI", 14F);
+            Margin = new Padding(5);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+    }
+}
---
+++ FormlarArasi_VeriTasima/FormlarArasi_VeriTasima/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormlarArasi_VeriTasima
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public string ad;
+        public string soyad;
+        public string meslek;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add(ad);
+            comboBox1.Items.Add(soyad);
+            comboBox1.Items.Add(meslek);
+
+        }
+    }
+}
